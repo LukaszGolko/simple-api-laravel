aa221998e9de9df4f3867f61f18e59901641e2dd
LukaszGolko <lukaszgolko26@gmail.com>
2025-06-28 17:54:23 +0200

    Cache and Observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Observers\CategoryObserver;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
 
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Category::observe(CategoryObserver::class);
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(6)->by($request->user()?->id ?: $request->ip());
         });
